067d9d9adec81a456844dd2d933056f4a57ab065
JulienUserCompanies <julien@usercompanies.com>
2026-04-15 13:44:45 +0100

    Run spamd in the background

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ set -e
 
 sa-update || true
 
-spamd --listen localhost --port 783 --pidfile /tmp/spamd.pid --create-prefs
+spamd --listen localhost --port 783 --pidfile /tmp/spamd.pid --create-prefs &
 
 exec uvicorn app:app --host 0.0.0.0 --port 8080
-EOF
