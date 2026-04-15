50dfd65c97a18a8dec7be054a31cf1651690ecd0
JulienUserCompanies <julien@usercompanies.com>
2026-04-15 12:59:36 +0100

    Update spamd to listen on 127.0.0.1 and use SPAMD_PORT

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 set -e
 
 PORT="${PORT:-10000}"
+SPAMD_PORT="${SPAMD_PORT:-1783}"
 
-spamd --listen localhost --port 783 --pidfile /tmp/spamd.pid --create-prefs
+spamd --listen 127.0.0.1 --port "$SPAMD_PORT" --pidfile /tmp/spamd.pid --create-prefs
 
 exec uvicorn app:app --host 0.0.0.0 --port "$PORT"
