241a7bf1198d617869e8b74a9c0d569998ef23da
JulienUserCompanies <julien@usercompanies.com>
2026-04-15 13:31:23 +0100

    Update start.sh to change port and add spamd

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-PORT="${PORT:-10000}"
+sa-update || true
 
-exec uvicorn app:app --host 0.0.0.0 --port "$PORT"
+spamd --listen localhost --port 783 --pidfile /tmp/spamd.pid --create-prefs
+
+exec uvicorn app:app --host 0.0.0.0 --port 8080
+EOF
