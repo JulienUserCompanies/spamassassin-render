3f89f54181f58629174f8c6148533f9e3f914f5a
JulienUserCompanies <julien@usercompanies.com>
2026-04-15 13:11:39 +0100

    Simplify start.sh by removing SPAMD_PORT
    Removed SPAMD_PORT variable and related command.

--- start.sh
+++ start.sh
@@ -2,8 +2,5 @@
 set -e
 
 PORT="${PORT:-10000}"
-SPAMD_PORT="${SPAMD_PORT:-1783}"
-
-spamd --listen 127.0.0.1 --port "$SPAMD_PORT" --pidfile /tmp/spamd.pid --create-prefs
 
 exec uvicorn app:app --host 0.0.0.0 --port "$PORT"
