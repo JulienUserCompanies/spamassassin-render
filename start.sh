6e0325563035573df18548d3af0e04f8127227a1
JulienUserCompanies <julien@usercompanies.com>
2026-04-15 12:17:11 +0100

    Fix uvicorn command to specify app module

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+PORT="${PORT:-10000}"
+
 spamd --listen localhost --port 783 --pidfile /tmp/spamd.pid --create-prefs
 
-exec uvicorn app --host 0.0.0.0 --port $PORT
+exec uvicorn app:app --host 0.0.0.0 --port "$PORT"
